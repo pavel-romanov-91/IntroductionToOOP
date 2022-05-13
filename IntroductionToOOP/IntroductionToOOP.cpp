5e5c0f96e1f4b6fa82e860bbc2166e0bdadccfa5
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-05-13 09:20:14 +0300

    Add project files.

---
+++ IntroductionToOOP/IntroductionToOOP.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+
+//#define STRUCT_POINT
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
+	//type name;
+	int a; // обьявление переменной а типа 'int'
+	Point A;//обьявления переменной A типа 'Point' 
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+	cout << (*pA).x << "\t" << (*pA).y << endl;
+
+	cout << sizeof(Point) << endl;
+	cout << typeid(A).name() << endl;
+#endif // STRUCT_POINT
+
+	Point A;
+	double x = 2, y = 3;
+	cout << "Введите координаты точки: "; cin >> x >> y;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+}
