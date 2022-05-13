36aa937eb0b3375a060e1726ddcfe3c4a6dee10a
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-05-13 14:21:20 +0300

    IntroductionToOOP

--- IntroductionToOOP/IntroductionToOOP.cpp
+++ IntroductionToOOP/IntroductionToOOP.cpp
@@ -22,7 +22,16 @@ public:
 	{
 		this->y = y;
 	}
+	double distance(Point dot)
+	{
+		double x_distance = dot.x - this->x;
+		double y_distance = dot.y - this->y;
+		double distance = (x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
 };
+
+double distance(Point A, Point B);
 
 //#define STRUCT_POINT
 
@@ -47,7 +56,7 @@ void main()
 
 	Point A;
 	double x = 2, y = 3;
-	cout << "Введите координаты точки: "; cin >> x >> y;
+	//cout << "Введите координаты точки: "; cin >> x >> y;
 	A.set_x(2);
 	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
@@ -55,4 +64,15 @@ void main()
 	B.set_x(7);
 	B.set_y(8);
 	cout << B.get_x() << "\t" << B.get_y() << endl;
+	cout << "Растояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << "Растояние между точки А и В: " << A.distance(B) << endl;
+	
+}
+
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = (x_distance * x_distance + y_distance * y_distance);
+	return distance;
 }
