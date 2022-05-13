bf9e2ff6d99937dfe76020f0c7093df2eac6a23e
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-05-13 22:05:22 +0300

    IntroductionToOOP

--- IntroductionToOOP/IntroductionToOOP.cpp
+++ IntroductionToOOP/IntroductionToOOP.cpp
@@ -22,18 +22,36 @@ public:
 	{
 		this->y = y;
 	}
+	//			Constructors:
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+
+	//			Methods
 	double distance(Point other)
 	{
-		double x_distance = other.x - this->x;
-		double y_distance = other.y - this->y;
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
 		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
 		return distance;
+	}
+	void print()const
+	{
+		cout << " X = " << x << "\tY = " << y << endl;
 	}
 };
 
 double distance(Point A, Point B);
 
 //#define STRUCT_POINT
+//#define DISTANC
 
 void main()
 {
@@ -54,6 +72,7 @@ void main()
 	cout << typeid(A).name() << endl;
 #endif // STRUCT_POINT
 
+#ifdef DISTANC
 	Point A;
 	double x = 2, y = 3;
 	//cout << "Введите координаты точки: "; cin >> x >> y;
@@ -65,8 +84,15 @@ void main()
 	B.set_y(8);
 	cout << B.get_x() << "\t" << B.get_y() << endl;
 	cout << "Растояние от точки А до точки В: " << A.distance(B) << endl;
-	cout << "Растояние между точки А и В: " << A.distance(B) << endl;
-	
+	cout << "Растояние от точки B до точки A: " << B.distance(A) << endl;
+	cout << "Растояние между точки А и В:	  " << A.distance(B) << endl;
+	cout << "Растояние между точки B и A:	  " << B.distance(A) << endl;
+#endif // DISTANC
+
+	int a;
+
+	Point A;
+	A.print();
 }
 
 double distance(Point A, Point B)
