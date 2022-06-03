295cf72567d8eefb563dde94b0898d4f734b471f
pavel-romanov-91 <101830399+pavel-romanov-91@users.noreply.github.com>
2022-06-03 22:08:25 +0300

    Methods out of class

--- Class_String/Class_String.cpp
+++ Class_String/Class_String.cpp
@@ -4,6 +4,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+
 #define delimiter "\n-------------------------------\n";
 
 class String
@@ -11,98 +12,120 @@ class String
 	int size;
 	char* str;
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
 
 	//				Constructors:
 
-	explicit String(int size = 80) :size(size), str(new char[size] {})
-	{
-		//this->size = size;
-		//this->str = new char[size] {};
-		cout << "DeflaConstruct:\t" << this << endl;
-	}
-	String(const char* str) :String(strlen(str)+1)
-	{
-		//this->size = strlen(str) + 1;
-		//this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = str[i];
-		cout << "Constructor:\t" << this << endl;
-	}
-	String(const String& other) :String(other.str)
-	{
-		//this->size = other.size;
-		//this->str = new char[size]{};
-		
-		cout << "CopyConstructor:" << this << endl;
-	}
-	String(String&& other) :size(other.size), str(other.str)
-	{
-		//					MoveConstructor выполняет ShellowCopy (поверхностное копирование)
-		//this->size = other.size;
-		//this->str = other.str;
-		other.size = 0;
-		other.str = nullptr;
-		cout << "MoveConstructor:" << this << endl;
-	}
-	~String()
-	{
-		delete[] this->str;
-		cout << "destructor:\t" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	String(String&& other);
+	~String();
 
 	//				Operators:
 
-	String& operator=(const String& other)
-	{
-		/*int a = 2;
-		int b = 3;
-		a = b;*/
-		if (this == &other)return *this;
-		delete[] this->str;
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
-	String& operator=(String&& other)
-	{
-		delete[] this->str;
-		this->size = other.size;
-		this->str = other.str;
-		other.size = 0;
-		other.str = nullptr;
-		cout << "MoveAssignment:\t" << this << endl;
-		return *this;
-	}
-	const char& operator[](int i)const
-	{
-		return str[i];
-	}
-	char& operator[](int i)
-	{
-		return str[i];
-	}
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	const char& operator[](int i)const;
+	char& operator[](int i);
 
 	//				Methods:
 
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 };
+int String::get_size()const
+{
+	return size;
+}
+const char*String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+//				Constructors:
+
+String::String(int size) :size(size), str(new char[size] {})
+{
+	//this->size = size;
+	//this->str = new char[size] {};
+	cout << "DeflaConstruct:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)
+{
+	//this->size = strlen(str) + 1;
+	//this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "Constructor:\t" << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	//this->size = other.size;
+	//this->str = new char[size]{};
+
+	cout << "CopyConstructor:" << this << endl;
+}
+String::String(String&& other) :size(other.size), str(other.str)
+{
+	//					MoveConstructor выполняет ShellowCopy (поверхностное копирование)
+	//this->size = other.size;
+	//this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+	cout << "MoveConstructor:" << this << endl;
+}
+String::~String()
+{
+	delete[] this->str;
+	cout << "destructor:\t" << this << endl;
+}
+
+//				Operators:
+
+String& String::operator=(const String& other)
+{
+	/*int a = 2;
+	int b = 3;
+	a = b;*/
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+String& String::operator=(String&& other)
+{
+	delete[] this->str;
+	this->size = other.size;
+	this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+	cout << "MoveAssignment:\t" << this << endl;
+	return *this;
+}
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+//				Methods:
+
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
 
 String operator+(const String& left, const String& right)
 {
